4e7a1d2bf80521553d1e7dcbe84293b9b8721a34
Sainathreddy <moolasainathreddy@gmail.com>
2023-03-24 02:12:58 +0000

    .gitignore

--- mysqlbackup.sh
+++ mysqlbackup.sh
@@ -1,1 +1,6 @@
-mysqldump -h db_host.rds.aws.com -u dbuser -p welcome db_flipkart > /db-backup/db_flipkart.sql
+v_dbName=$1
+v_password=$(cat cred | grep password|awk -F"=" '{print $2}')
+v_username=$(cat cred | grep username|awk -F"=" '{print $2}')
+v_dbHost=db_host.rds.aws.com
+
+echo "mysqldump -h $v_dbHost -u $v_username -p $v_dbhost $v_dbName > /db-backup/$v_dbName.sql"
